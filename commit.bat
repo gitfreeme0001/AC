778a2f55104584828351031d38928a8330b863a0
atsus <atsushinee@outlook.com>
2020-07-01 19:43:38 +0800

    auto commit date

--- commit.bat
+++ commit.bat
@@ -1,7 +1,7 @@
 @echo off
 for /l %%i in (1,1,30) do (
-    date 2020/8/%%i
-    echo 2020/8/%%i > .AC
+    date 2020/7/%%i
+    echo 2020/7/%%i > .AC
     git add .
     git commit -m "auto commit date"
 )
